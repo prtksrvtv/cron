b451ebfbbe4e5c9fcdd6e04020a764339d6316f9
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-02-02 12:45:52 +0530

    printing in shell

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-subprocess.run(['curl', os.environ['MAIN_APP']], shell=True)
-subprocess.run(['curl', os.environ['DB_SEARCH_API']], shell=True)
-subprocess.run(['curl', os.environ['TELEGRAM_API']], shell=True)
-subprocess.run(['curl', os.environ['EMAILER_API']], shell=True)
+print(subprocess.run(['curl', os.environ['MAIN_APP']], shell=True))
+print(subprocess.run(['curl', os.environ['DB_SEARCH_API']], shell=True))
+print(subprocess.run(['curl', os.environ['TELEGRAM_API']], shell=True))
+print(subprocess.run(['curl', os.environ['EMAILER_API']], shell=True))
