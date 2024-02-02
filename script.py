8ad7b5431941eae29b35de162692036067c5df3e
Prateek Srivastava <prateeksrivastava9@gmail.com>
2024-02-02 13:38:21 +0530

    incorporated looping

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
-import subprocess
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-print(subprocess.run(['curl', os.environ['MAIN_APP']], shell=True))
-print(subprocess.run(['curl', os.environ['DB_SEARCH_API']], shell=True))
-print(subprocess.run(['curl', os.environ['TELEGRAM_API']], shell=True))
-print(subprocess.run(['curl', os.environ['EMAILER_API']], shell=True))
+cata={'Main App':os.environ['MAIN_APP'], 'DB Search API':os.environ['DB_SEARCH_API'], 'Telegram API':os.environ['TELEGRAM_API'], 'Emailer API':os.environ['EMAILER_API']}
+
+os.system("echo Draugar Cron Job")
+os.system("echo To keep the APIs and MicroServices Live")
+for x in cata:
+    os.system("echo Performing Curl on "+x)
+    os.system('curl '+ cata[x])
+    os.system("echo Success")
+
